837915ea35c4114d270ad7cb340b5903f4cc4c94
ErikaPOrtiz <erika0418p@gmail.com>
2025-04-03 12:47:45 -0600

    Create jenkisfile with github conexion to build stage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Erika Ortiz ID 456980, In class assesment', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Erika Ortiz ID 456980, In class assesment/i);
   expect(linkElement).toBeInTheDocument();
 });
